723f883930399414418be6f8a70243882318d0b6
Wander Moreira <wandersantosmoreira@gmail.com>
2021-11-24 16:22:16 -0300

    feat: groups models with tests

--- groups/models.py
+++ groups/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class Group(models.Model):
+    leader = models.ForeignKey('accounts.User', on_delete=models.PROTECT, related_name='leader')
+    name = models.CharField(max_length=150)
+
+    users = models.ManyToManyField('accounts.User', through='groups.GroupUser')
+
+class GroupUser(models.Model):
+    group = models.ForeignKey('groups.Group', on_delete=models.PROTECT)
+    user = models.ForeignKey('accounts.User', on_delete=models.PROTECT)
+    is_admin = models.BooleanField(default=False)
+
+class JoinGroupRequest(models.Model):
+    group = models.ForeignKey('groups.Group', on_delete=models.PROTECT)
+    user = models.ForeignKey('accounts.User', on_delete=models.PROTECT) 
