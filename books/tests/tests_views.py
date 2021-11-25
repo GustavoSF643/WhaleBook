ba63924609926319fc49563ef745be9623885813
Gustavo Silva <gustavosilvafranco643@gmail.com>
2021-11-25 16:56:51 -0300

    fix: tests adjusteds

--- books/tests/tests_views.py
+++ books/tests/tests_views.py
@@ -1,4 +1,3 @@
-from django.http import response
 from accounts.models import User
 from rest_framework.test import APITestCase
 from rest_framework_simplejwt.tokens import RefreshToken
@@ -10,37 +9,18 @@ class BookViewTest(APITestCase):
             password="12345678",
         )
         refresh = RefreshToken.for_user(self.user)
-        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + refresh.access_token)
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + str(refresh.access_token))
 
-    def user_can_list_books(self):
-        response = self.client.get('/api/books/')
-
-        self.assertEqual(response.status_code, 200)
-
-    def user_can_list_books_by_category(self):
-        category = 'Music'
-        response = self.client.get(f'/api/books/?category={category}')
+    def test_user_can_list_books(self):
+        response = self.client.get("/api/books/?q=''")
 
         self.assertEqual(response.status_code, 200)
 
-        for book in response.json():
-            for book_category in book['categories']:
-                self.assertEqual(bool(category in book_category), True)
-
-    def user_can_list_books_by_title(self):
-        title = 'Music'
-        response = self.client.get(f'/api/books/?title={title}')
-
-        self.assertEqual(response.status_code, 200)
-
-        for book in response.json():
-            book_info = book['volumeInfo']
-            self.assertEqual(bool(title in book_info['title']), True)
-
-    def user_can_access_a_specific_book(self):
-        books = self.client.get('/api/books/').json()
+    def test_user_can_access_a_specific_book(self):
+        books = self.client.get("/api/books/?q=''").json()
+        
         book = books[0]
-        book_url = book['book_url']
+        book_url = book['selfLink']
 
         response = self.client.get(book_url)
 
