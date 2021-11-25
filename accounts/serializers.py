2894d55f6c3680a321830d4959c4d5f2785f8cb9
Paulo Mello <rapha.mello87@gmail.com>
2021-11-24 23:34:19 -0300

    feat: serializers to user models (login, create, friends) created

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -1,1 +1,33 @@
-from django.db.models import fields
+from rest_framework import serializers
+from models import User
+
+
+class CreateUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'password',
+                  'is_staff', 'is_superuser', 'is_active']
+    
+    extra_kwargs = {
+        'is_active': {'read_only': True},
+        'password': {'write_only': True}
+    }
+
+
+class LoginUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+
+
+class FriendUserSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    is_active = serializers.BooleanField()
+
+
+class UserDataSerializer(serializers.ModelSerializer):
+    friends = FriendUserSerializer(many=True)
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'is_staff', 'is_superuser', 'is_active',
+                  'friends']
