e90714c08ec51ed0b78e1c88505efa2cdec3fd88
Wander Moreira <wandersantosmoreira@gmail.com>
2021-11-26 13:29:00 -0300

    feat: merged with the develop

--- capstone_django/urls.py
+++ capstone_django/urls.py
@@ -13,22 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# from django.contrib import admin
-# from django.urls import path, include
-
-# urlpatterns = [
-#     path('admin/', admin.site.urls),
-#     path('api/', include('books.urls')),
-#     path('api/', include('groups.urls')),
-# ]
-
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('token/', TokenObtainPairView.as_view()),
-    path('token/refresh', TokenRefreshView.as_view()),
+    path('api/', include('books.urls')),
     path('api/', include('groups.urls')),
 ]
+
+
