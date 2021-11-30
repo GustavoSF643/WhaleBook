0ed2b50d3a8594780eff6341408b5f42e42ed554
Gustavo Silva <gustavosilvafranco643@gmail.com>
2021-11-30 16:13:40 -0300

    feat: email field adjusted

--- accounts/models.py
+++ accounts/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class User(AbstractUser):
     friends = models.ManyToManyField('accounts.User')
+    email = models.EmailField(max_length=254, unique=True)
 
 
 class UserBooks(models.Model):
